c14cca41806b392867b5eea56076fdf5910823c6
kostyukai <kostyukai0301@yandex.ua>
2014-03-29 12:07:10 +0300

    Create task_10.s

---
+++ hw-1/task_10.s
@@ -0,0 +1,63 @@
+.data
+
+fmt_char:
+	.space 1
+fmt_short:
+        .space 2
+fmt_int:
+        .space 4
+fmt_double:
+        .space 8
+
+fmt_sc:
+	.string "%c %hd %d %lf"
+fmt_pr_c:
+	.string "%c "
+fmt_pr_s:
+        .string "%hd "
+fmt_pr_i:
+        .string "%d "
+fmt_pr_d:
+        .string "%lf\n"
+.text
+.globl main
+
+main:
+	pushl %ebp
+	movl %esp, %ebp
+	
+	pushl $fmt_double
+	pushl $fmt_int
+	pushl $fmt_short
+	pushl $fmt_char
+	pushl $fmt_sc
+	call scanf
+//char
+	movb fmt_char, %al
+	pushl %eax
+	 pushl $fmt_pr_c
+        call printf
+//short
+	   movw fmt_short, %ax
+        pushl %eax
+         pushl $fmt_pr_s
+        call printf
+//int
+	   movl fmt_int, %eax
+        pushl %eax
+         pushl $fmt_pr_i
+        call printf
+//double
+	   	fldl fmt_double
+movl $fmt_pr_d, %eax
+fstpl	4(%esp)
+movl %eax, (%esp)
+call printf
+//pushl $pr
+//call printf
+
+	movl $0, %eax
+	movl %ebp, %esp	
+	popl %ebp
+	ret
+
