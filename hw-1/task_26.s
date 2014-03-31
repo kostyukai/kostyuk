4591c696cdd05d032eb8c3a2f8bb14ed69221f50
kostyukai <kostyukai0301@yandex.ua>
2014-03-31 18:32:20 +0300

    Create task_26.s

---
+++ hw-1/task_26.s
@@ -0,0 +1,40 @@
+.data
+fmt_scan:
+        .string "%s"
+fmt_print:
+        .string "%d\n"
+str:
+        .space 10
+.text
+.globl main
+main:
+        pushl %ebp
+        movl %esp, %ebp
+
+
+        pushl $str
+        pushl $fmt_scan
+        call scanf
+        addl $8, %esp
+
+        movl $str, %ebx
+start:
+        incl %ebx
+        addl $1, %eax
+        cmpl $0 , (%ebx)
+        jne start
+
+
+        decl %eax
+        decl %eax
+
+
+        pushl %eax
+        pushl $fmt_print
+        call printf
+
+        addl $8, %esp
+        movl $0, %eax
+        movl %ebp, %esp
+        popl %ebp
+        ret
