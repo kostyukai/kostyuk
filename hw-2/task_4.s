001148ecd997904a49be1aeaaf12a45aff6dee4b
kostyukai <kostyukai0301@yandex.ua>
2014-05-14 19:05:59 +0400

    Update task_4.s

--- hw-2/task_4.s
+++ hw-2/task_4.s
@@ -1,49 +1,76 @@
 #x>0 !!!
         
-        .data
-fmt_str:
-        .string "%f"
+.data
+
 x:
-        .space 4
+.space 4
 y:
-        .space 4
-        .text
-        .globl main
+.space 4
+
+str_fmt:
+.string "%f"
+
+str_fmt2:
+.string "%.2f"
+
+str_ent:
+.string "\n"
+
+.text
+.globl main
+
 main:
-        pushl %ebp
-        movl %esp, %ebp
+pushl %ebp
+movl %esp, %ebp
 
-        //subl $16, %esp
+pushl $x
+pushl $str_fmt
+call scanf
+addl $8, %esp
 
-        pushl $x
-        pushl $fmt_str
-        call scanf
+pushl $y
+pushl $str_fmt
+call scanf
+addl $8, %esp
 
-        pushl $y
-        pushl $fmt_str
-        call scanf
+flds y
+flds x
+#2^t, t = round (log2 x^y)
+fyl2x
+frndint
+fld1
+fscale
+flds y
+flds x
+fyl2x
+frndint
 
-        movl x, %eax
-        movl y, %ebx
+#p = log2x^y - round (log2x^y)
+flds y
+flds x
+fyl2x
+fsubp
 
-        cmpl $0, %ebx
-        je   m1
-
-        flds y
-        flds x
-
-        fyl2x
-
-        fstpl (%esp)
-        pushl $fmt_str
-        call printf
+#2^p-1
+f2xm1
 
 
+fld1
+faddp
 
+#st(0) 2^(p+t) = 2^(log2 x^y) = x^y
+fmulp
 
-        movl $0, %eax
-        movl %ebp, %esp
-        popl %ebp
-        ret
-        
-FRNDINT ОКРУГЛЕНИЕ
+fstpl (%esp)
+pushl $str_fmt2
+call printf
+addl $8, %esp
+
+pushl $str_ent
+call printf
+addl $4, %esp
+
+movl $0, %eax
+movl %ebp, %esp
+popl %ebp
+ret
