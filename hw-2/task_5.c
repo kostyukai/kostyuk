6daf0c31c4895d6159d1f4f2fa3fa424b399dd8e
kostyukai <kostyukai0301@yandex.ua>
2014-05-14 19:13:32 +0400

    Create task_5.c

---
+++ hw-2/task_5.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int intsum(int *a, int n);
+float floatsum(float *f, int n);
+int main()
+{
+int i;
+int n = 5;
+int *a = (int *)malloc(n*sizeof(int));
+float *f = (float *)malloc(n*sizeof(float));
+for (i=0; i<5; i++)
+scanf("%d", &a[i]);
+printf("%d\n", intsum(a, n));
+for (i=0; i<5; i++)
+scanf("%f", &f[i]);
+printf("%f\n", floatsum(f, n));
+
+return 0;
+}
