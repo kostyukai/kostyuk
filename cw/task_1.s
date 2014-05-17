0de007b32221c713ebc49c020df6baac3c4eb895
kostyukai <kostyukai0301@yandex.ua>
2014-05-17 12:50:52 +0400

    Move

---
+++ cw/task_1.s
@@ -0,0 +1,29 @@
+   .data
+msg_string1:
+        .string "Call_1.\n"
+
+
+msg_string2:
+        .string "Call_2.\n"
+
+
+        .text
+        .globl main
+my_printf :
+        pushl $msg_string1
+        call printf
+         addl    $4, %esp
+
+        pushl $msg_string2
+ call printf
+         addl    $4, %esp
+        ret
+main:
+        pushl %ebp
+        movl %esp, %ebp
+        call my_printf
+        movl    %ebp, %esp
+        popl    %ebp
+        ret
+
+
